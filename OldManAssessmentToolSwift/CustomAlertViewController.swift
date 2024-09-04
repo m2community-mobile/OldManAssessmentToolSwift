3f382b7bc74f19687aa8d1c0f83ea60222879315
M2 <m2community.mobile@gmail.com>
2024-09-04 09:34:49 +0900

    회원 탈퇴 commit

---
+++ OldManAssessmentToolSwift/CustomAlertViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CustomAlertViewController.swift
+//  OldManAssessmentToolSwift
+//
+//  Created by m2comm on 2024/09/03.
+//  Copyright © 2024 JinGu's iMac. All rights reserved.
+//
+
+import Foundation
---
+++ OldManAssessmentToolSwift/DeletedInfoViewController.swift
@@ -0,0 +1,9 @@
+//
+//  DeletedInfoViewController.swift
+//  OldManAssessmentToolSwift
+//
+//  Created by m2comm on 2024/09/02.
+//  Copyright © 2024 JinGu's iMac. All rights reserved.
+//
+
+import Foundation
